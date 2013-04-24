0c130f484ed855386bbdd3a130e654e98ef3b86d
Michael Schwartz <mykesx@gmail.com>
2013-04-24 09:31:26 -0700

    Finished release.sh
    Not fully working

--- release.sh
+++ release.sh
@@ -1,11 +1,30 @@
 #!/bin/sh
 
-rm -rf release
-mkdir release
-#cd docroot/mobile
-#echo "Building production version of the mobile app"
-#sencha app build production -d ../../release/mobile 
-#cd ../..
+TMPDIR=build-tmp
+
+cd docroot/mobile
+echo "Building production version of the mobile app"
+time sencha app build production >/dev/null 2>&1
+cd ../..
 echo "Building production version of the desktop app"
 cd docroot/desktop
-sencha --debug app build production -d ../../release/desktop 
+time sencha app build production >/dev/null 2>&1
+cd ../..
+echo "Creating document root layout"
+rm -rf $TMPDIR
+mkdir $TMPDIR
+
+cp -rp docroot/img $TMPDIR
+cp -rp docroot/index.html $TMPDIR
+cp -rp docroot/json $TMPDIR
+cp -rp docroot/desktop/build/desktop/production $TMPDIR/desktop
+cp -rp docroot/mobile/build/mobile/production $TMPDIR/mobile
+
+echo "Creating zip file"
+cd $TMPDIR
+zip -rq ../release.zip .
+cd ..
+rm -rf $TMPDIR
+ls -l release.zip
+
+echo "done"
